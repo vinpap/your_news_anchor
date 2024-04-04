67c623f90ce576f5d9e3adfccd7d6fe9f6a78bb0
Vincent <papelardvincent@gmail.com>
2024-04-04 14:24:37 +0200

    Reorganizing the repo

--- extract_articles.py
+++ extract_articles.py
@@ -5,6 +5,7 @@ This script extracts and filters news articles from a list of RSS feeds and
 saves them using a provided API. It is meant to be run at regular intervals, 
 e.g. once per day, so that the saved news articles stay up-to-date.
 """
+
 import os
 import logging
 
@@ -16,35 +17,47 @@ from extraction_functions import parse_rss_feed, get_sources_list
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
     logger = logging.getLogger()
     logger.info("Starting news articles extraction...")
     config = yaml.safe_load(open("./config.yml"))
-    
+
     # Getting API key fron environment values
     config["security_token"] = os.environ["API_TOKEN"]
 
     logger.info(f"""Database API endpoint: {config["api"]}""")
-    logger.info(f"""Max number of articles to scrap per source: {config["max_articles_per_source"]}""")
+    logger.info(
+        f"""Max number of articles to scrap per source: {config["max_articles_per_source"]}"""
+    )
 
     # Retrieving the list of RSS feeds from the API
-    rss_sources = get_sources_list(config["api"] + "/feeds")
-    
+    rss_sources = get_sources_list(config["api"] + "/all_feeds")
+    print(rss_sources)
 
     scraped_articles = []
 
     for source in rss_sources:
         logger.info(f"""Extracting articles from RSS feed {source["url"]}""")
-        results = parse_rss_feed(source_id=source["source_id"], source_name=source["name"], feed_path=source["url"], articles_max_count=config["max_articles_per_source"])
+        results = parse_rss_feed(
+            source_id=source["source_id"],
+            source_name=source["name"],
+            feed_path=source["url"],
+            articles_max_count=config["max_articles_per_source"],
+            is_from_user=source["is_from_user"],
+        )
         scraped_articles.extend(results)
-    
+
     logger.info("Articles extraction complete!")
 
     logger.info("Saving all articles...")
-    response = requests.post(config["api"] + "/update_articles", json={"articles": scraped_articles, "security_token": config["security_token"]})
+    response = requests.post(
+        config["api"] + "/update_articles",
+        json={"articles": scraped_articles, "security_token": config["security_token"]},
+    )
     if response.status_code != 200:
         logger.error(f"Update of daily articles failed. Response: {response.text}")
     else:
         logger.info("Save complete!")
-
--- extraction_functions.py
+++ extraction_functions.py
@@ -1,15 +1,24 @@
+import os
 import logging
 
 import feedparser
 import newspaper
 import requests
 
-# Spacy's named entity detection is used to filter the authors' names by keeping 
+# Spacy's named entity detection is used to filter the authors' names by keeping
 # people's name only
 import spacy
+
 nlp = spacy.load("fr_core_news_lg")
 
-def parse_rss_feed(source_id: int, source_name: str, feed_path: str, articles_max_count=10) -> list:
+
+def parse_rss_feed(
+    source_id: int,
+    source_name: str,
+    feed_path: str,
+    articles_max_count=10,
+    is_from_user=False,
+) -> list:
     """
     Retrieves the RSS feed located at feed_path and returns a dictionary that
     contains the n first articles along with their URL and their title.
@@ -20,35 +29,37 @@ def parse_rss_feed(source_id: int, source_name: str, feed_path: str, articles_ma
     - source_name: the RSS source name, also retrieved from the API
     - feed_path: the URL (or filepath) to the RSS feed
     - articles_max_count: the max number of articles to extract from the RSS
-    feed. This function will extract the articles content in their order in the 
-    RSS feed, until 'articles_max_count' articles have been extracted or the end 
+    feed. This function will extract the articles content in their order in the
+    RSS feed, until 'articles_max_count' articles have been extracted or the end
     of the feed has been reached.
+    - is_from_user: whether the news source has been added by an user or is a standard one
 
     Returns a list of dictionaries that include the following elements:
     - 'title': title of the article
     - 'content': content of the article
-    - 'source': the name of the website the news originate from (equal to 
+    - 'source': the name of the website the news originate from (equal to
     'source_name')
-    - 'source_id': the unique ID given to the source, as provided by the 
+    - 'source_id': the unique ID given to the source, as provided by the
     'source_id' parameter
     - 'url': the URL where the article can be found.
     """
     articles = []
 
-    news_feed = feedparser.parse(feed_path) # Parsing the RSS feed
+    news_feed = feedparser.parse(feed_path)  # Parsing the RSS feed
     scraped_articles = 0
     for item in news_feed.entries:
         article_data = {
             "title": item.title,
             "url": item.link,
             "source": source_name,
-            "source_id": source_id
+            "source_id": source_id,
+            "is_from_user": is_from_user,
         }
         try:
             parsed_article = parse_article_webpage(item.link)
-            
+
             # The function call below makes sure that the article content
-            # is relevant and can be used, cf function docstring for further 
+            # is relevant and can be used, cf function docstring for further
             # info
             if not content_is_relevant(parsed_article["text"]):
                 # Irrelevant entries in the RSS feed are simply ignored
@@ -56,7 +67,7 @@ def parse_rss_feed(source_id: int, source_name: str, feed_path: str, articles_ma
 
             article_data["content"] = parsed_article["text"]
 
-            # Authors name are filtered in order to get rid of some bad 
+            # Authors name are filtered in order to get rid of some bad
             # detections, e.g. errors caused by an improper use of capital
             # letters or point
             article_data["authors"] = parsed_article["authors"]
@@ -73,8 +84,9 @@ def parse_rss_feed(source_id: int, source_name: str, feed_path: str, articles_ma
         scraped_articles += 1
         if scraped_articles >= articles_max_count:
             break
-        
+
     return articles
+
 
 def parse_article_webpage(url: str) -> dict:
     """
@@ -89,23 +101,23 @@ def parse_article_webpage(url: str) -> dict:
         "authors": article.authors,
     }
     try:
-        response_object["date"] = article.publish_date.strftime('%a %d %b %Y, %I:%M%p')
+        response_object["date"] = article.publish_date.strftime("%a %d %b %Y, %I:%M%p")
     # Accounting for the case where no date was found
     except AttributeError:
         response_object["date"] = ""
-    
+
     try:
         response_object["image"] = article.top_image
     # In case no image was found
     except:
         response_object["image"] = ""
-    
 
     return response_object
 
+
 def filter_authors(authors: list) -> list:
     """
-    Filters the authors for an article in order to get rid of errors during 
+    Filters the authors for an article in order to get rid of errors during
     authors detection.
 
     In order to do that, named-entity detection (NER) is run on the list of
@@ -117,8 +129,9 @@ def filter_authors(authors: list) -> list:
     for word in entities.ents:
         if word.label_ == "PER":
             filtered_authors.append(word.text)
-    
+
     return filtered_authors
+
 
 def content_is_relevant(article: str) -> bool:
     """
@@ -129,7 +142,7 @@ def content_is_relevant(article: str) -> bool:
     - Video reports, where the actual news content is presented in a video and
     the text content only serves as a caption.
     - Ads that were improperly detected as news articles by newspaper.Article
-    - Articles that are too short too extract any meaningful recaps, such as 
+    - Articles that are too short too extract any meaningful recaps, such as
     live newsfeeds content for example.
     - Website banners prompting the reader to subscribe to the newspaper, which
     are sometimes improperly detected as article content.
@@ -142,7 +155,7 @@ def content_is_relevant(article: str) -> bool:
     # were not properly processed by newspaper.Article.
     if len(article) < 500:
         return False
-    
+
     return True
 
 
@@ -151,5 +164,5 @@ def get_sources_list(endpoint: str) -> list:
     Returns the list of RSS feeds to get articles from in the database.
     """
     logging.getLogger().info("Retrieving RSS feeds list from API endpoint")
-    sources = requests.get(endpoint)
+    sources = requests.post(endpoint, params={"secret_token": os.environ["API_TOKEN"]})
     return sources.json()
